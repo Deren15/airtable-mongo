eb80c150c3bfdf25eef56dc4af0127eacd24a5fa
Deren Y <67863795+Deren15@users.noreply.github.com>
2023-03-03 17:31:08 +0530

    Update NewsArticle.js

--- models/NewsArticle.js
+++ models/NewsArticle.js
@@ -1,26 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const newsArticleSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    banner_image: {
-        type: String,
-        required: true,
-    },
-    article: {
-        type: String,
-        required: true,
-    },
-    categories: {
-        type: Array,
-    },
-    summary: {
-        type: String,
-    }
-}, {timestamps: true});
+const newsArticleSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		slug: {
+			type: String
+		},
+		banner_image: {
+			type: String,
+			required: true,
+		},
+		article: {
+			type: String,
+			required: true,
+		},
+		categories: {
+			type: Array,
+		},
+		summary: {
+			type: String,
+		},
+		link: {
+			type: String,
+		},
+		timestamp: {
+			type: String,
+		},
+	},
+	{ timestamps: true }
+);
 
-const NewsArticle = mongoose.model('NewsArticle', newsArticleSchema);
-module.exports = NewsArticle
+const NewsArticle = mongoose.model("NewsArticle", newsArticleSchema);
+module.exports = NewsArticle;
